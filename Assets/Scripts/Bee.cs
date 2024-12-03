7297bfe68727f91f339e6dd10ca3124b1b2b3719
Matt <mm7f5@umsystem.edu>
2024-12-02 22:47:52 -0600

    got some basic mouse following

--- Assets/Scripts/Bee.cs
+++ Assets/Scripts/Bee.cs
@@ -1,18 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Bee : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody _rb;
+    
+
+
+    public float moveSpeed = 10f;
+    
+    private Camera _mainCamera;
+
+    private void Awake()
     {
-        
+        _rb = GetComponent<Rigidbody>();
+        _mainCamera = Camera.main;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private Vector3 GetMouseWorldPosition()
     {
-        
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = _mainCamera.nearClipPlane;
+        return _mainCamera.ScreenToWorldPoint(mousePosition);
     }
+    
+
+    void FixedUpdate()
+    {
+        Vector3 targetPosition = GetMouseWorldPosition();
+        Vector3 currentPosition = _rb.position;
+        
+        Debug.Log(currentPosition);
+        Vector3 newPosition = Vector3.MoveTowards(currentPosition, targetPosition, moveSpeed * Time.fixedDeltaTime);
+        _rb.MovePosition(newPosition);
+    }
+
+    // private Vector3 ProcessMovement()
+    // {
+    //     _targetPosition = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
+    //     _currentPosition = Vector3.MoveTowards(_currentPosition, _targetPosition, moveSpeed * Time.deltaTime);
+    //     return _currentPosition;
+    // }
 }
